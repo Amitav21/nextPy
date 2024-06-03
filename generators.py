29f2b692059998c5a7c6e9d33d888c9bdf03eec9
Amitav21 <avivamit4u@gmail.com>
2024-06-03 22:18:44 +0300

    created a function that returns a generator of times

--- generators.py
+++ generators.py
@@ -1,3 +1,5 @@
+import itertools
+
 def gen_secs():
     for i in range (60):
         yield i
@@ -10,11 +12,12 @@ def gen_hours():
     for i in range (24):
         yield i
 
+def gen_time():
+    for hour in gen_hours():
+        for minute in gen_minutes():
+            for second in gen_secs():
+                yield f"{hour:02}:{minute:02}:{second:02}"
 
 if __name__ == "__main__":
-    for second in gen_secs():
-        print(second)
-    for minute in gen_minutes():
-        print(minute)
-    for hour in gen_hours():
-        print(hour)
+    for time in gen_time():
+        print(time)

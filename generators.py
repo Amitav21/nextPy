74342d492a1706f58584882932503cb91164e6bb
Amitav21 <avivamit4u@gmail.com>
2024-06-03 22:29:42 +0300

    created a function that returns a generator of all the days in a specific month according to if its a leap year

--- generators.py
+++ generators.py
@@ -29,6 +29,14 @@ def gen_months():
     for i in range (1,13):
         yield i
 
+def gen_days(month, leap_year=True):
+    month_to_days = {1:31, 2:28, 2.5:29, 3:31, 4:30, 5:31, 6:30,
+        7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    if leap_year and month == 2:
+        month = 2.5
+    for day in range(1, month_to_days[month] + 1):
+        yield day
+
 if __name__ == "__main__":
-    for month in gen_months():
-        print(month)
+    for day in gen_days(2,False):
+        print(day)

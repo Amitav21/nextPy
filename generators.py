73354a85570936fea8440cd91bea131d192d2416
Amitav21 <avivamit4u@gmail.com>
2024-06-03 22:09:37 +0300

    created a function that returns a generator of minutes

--- generators.py
+++ generators.py
@@ -2,7 +2,13 @@ def gen_secs():
     for i in range (59):
         yield i
 
+def gen_minutes():
+    for i in range (59):
+        yield i
+
 
 if __name__ == "__main__":
     for second in gen_secs():
         print(second)
+    for minute in gen_minutes():
+        print(minute)

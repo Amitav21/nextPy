15ddb1bef51955bf4db6e4236ac3d7befe986091
Amitav21 <avivamit4u@gmail.com>
2024-06-04 23:47:48 +0300

    created an IDIterator class that recieves a starting id and iterating between the valid id's

--- iterators.py
+++ iterators.py
@@ -15,6 +15,24 @@ def check_id_valid(id_number):
         return False
     return True
 
+
+
+class IDIterator:
+
+    def __init__(self, _id):
+        self._id = _id
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while not check_id_valid(self._id):
+            self._id += 1
+            if self._id == 999999999:
+                raise StopIteration
+        return self._id
+
+
 if __name__ == "__main__":
     print(check_id_valid(123456780))
     print(check_id_valid(123456782))
